a2115552d80ef92ff37469e2c3e71ba0109405ab
denis615 <58919732+denis615@users.noreply.github.com>
2019-12-21 23:49:03 +0100

    Create javascript.js

---
+++ Functions Arrays Homework/javascript.js
@@ -0,0 +1,71 @@
+
+
+//Homework Part 1
+
+let _strings=["Denis","Happy","a Hamburger"];
+
+
+function tellStory(){
+    alert(`This is ${_strings[0]} he is a nice person. Today he is ${_strings[1]}. 
+    Because he ate ${_strings[2]} `);
+
+}
+
+tellStory()
+
+
+//Homework Part 2
+
+
+let= _arrayofnumbers=[5,7,4,7,"Hello"];
+function sum(){
+    
+    alert( _arrayofnumbers[0] + _arrayofnumbers[1] + _arrayofnumbers[2] + _arrayofnumbers[3]+_arrayofnumbers[4]);
+};
+
+sum()
+
+function validateNumber(){
+   
+ if (typeof _arrayofnumbers[0]==`number`) {
+     alert(`That Element it is a number with the value of ${_arrayofnumbers[0]}`)
+     
+ } 
+     
+ }
+   
+ if ( typeof _arrayofnumbers[1]==`number`) {
+    alert(`That Element is is a number with the value of ${_arrayofnumbers[1]}`)
+    
+} 
+    
+    
+
+if ( typeof _arrayofnumbers[2]==`number`) {
+    alert(`That Element is is a number with the value of ${_arrayofnumbers[2]}`)
+    
+} 
+
+    
+
+
+
+if ( typeof _arrayofnumbers[3]==`number`) {
+    alert(`That Element is is a number with the value of ${_arrayofnumbers[3]}`)
+    
+} 
+    
+
+
+if ( typeof _arrayofnumbers[4]==`number`) {
+    alert(`That Element is is a number with the value of ${_arrayofnumbers[4]}`)
+    
+} else { alert("Error that element in the array is invalid")
+    
+}
+
+
+
+
+validateNumber()
+
