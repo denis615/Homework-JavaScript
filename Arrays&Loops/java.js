756b023e2fef5d12085710edbf3633464df07247
denis615 <58919732+denis615@users.noreply.github.com>
2019-12-23 00:49:50 +0100

    Create java.js

---
+++ Arrays&Loops/java.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+// OK so there is a javascript function to do this, so I am doing it the easy way :)
+
+
+let _string1=["Hello","My master","is","very","lazy","so","he","used","the","easy","way"];
+
+function ArrayofStrings(){
+
+alert(`${_string1.join(`,`)}`)
+
+}
+
+ArrayofStrings()
